7a1a7275ab32ff6dc7b8f5b2900808a6f02f84d9
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2026-08-03 18:18:16 -0700

    hide comb recipes in emi

--- src/main/java/com/accbdd/complex_bees/compat/emi/ComplexBeesEMI.java
+++ src/main/java/com/accbdd/complex_bees/compat/emi/ComplexBeesEMI.java
@@ -1,12 +1,18 @@
 package com.accbdd.complex_bees.compat.emi;
 
 import com.accbdd.complex_bees.datagen.bee.ComplexSpecies;
+import com.accbdd.complicated_bees.ComplicatedBees;
 import com.accbdd.complicated_bees.bees.Species;
+import com.accbdd.complicated_bees.block.BeeNestBlock;
+import com.accbdd.complicated_bees.compat.emi.recipe.BeeProduceEmiRecipe;
+import com.accbdd.complicated_bees.registry.ItemsRegistration;
+import com.accbdd.complicated_bees.registry.SpeciesRegistration;
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.stack.EmiStack;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 
 @EmiEntrypoint
 public class ComplexBeesEMI implements EmiPlugin {
@@ -17,7 +23,14 @@ public class ComplexBeesEMI implements EmiPlugin {
             if (BuiltInRegistries.ITEM.getTag(entry.tag()).isEmpty()) {
                 Species species = entry.species();
                 species.toMembers().forEach(stack -> registry.removeEmiStacks(EmiStack.of(stack)));
-                //registry.removeEmiStacks(EmiStack.of(BeeNestBlock.stackNest(ItemsRegistration.BEE_NEST.get().getDefaultInstance(), species)));
+                registry.removeEmiStacks(EmiStack.of(BeeNestBlock.stackNest(ItemsRegistration.BEE_NEST.get().getDefaultInstance(), SpeciesRegistration.getFromResourceLocation(entry.key().location()))));
+                //fix this, maybe very fragile?
+                registry.removeRecipes(recipe -> {
+                    if (recipe instanceof BeeProduceEmiRecipe produce) {
+                        return produce.getId().equals(ResourceLocation.fromNamespaceAndPath(ComplicatedBees.MODID, "/bee_produce/" + entry.key().location().toString().replace(":", "/")));
+                    }
+                    return false;
+                });
             }
         }
     }
